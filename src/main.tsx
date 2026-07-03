50f845c4955b7e7e51fd0e76196904c6bee3661a
ACTECNICAL66 <alejandroucanton@gmail.com>
2026-07-03 00:18:35 -0300

    Fix: SPA routing 404 en GitHub Pages - agregar basename a BrowserRouter y 404.html redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import './index.css'
-import { TRPCProvider } from "@/providers/trpc"
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import { TRPCProvider } from "@/providers/trpc";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+const redirectTo = sessionStorage.getItem("redirectTo");
+if (redirectTo) {
+  sessionStorage.removeItem("redirectTo");
+  window.history.replaceState(null, "", redirectTo);
+}
+
+const basename = import.meta.env.BASE_URL.replace(/\/+$/, "");
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={basename}>
     <TRPCProvider>
       <App />
     </TRPCProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
